cb37de6429d6562aff692f32d54c3c4e005ad88e
Joao-Ureta <148173512+Joao-Ureta@users.noreply.github.com>
2023-11-30 01:02:19 -0300

    EPE3
    estaba dificil :c me falto tiempo

---
+++ EPE3CentroMedico/src/main/java/Logica/CentroMedico.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Logica;
+
+import Gui.IngresoDatos;
+import Gui.PrincipalCenter;
+import Gui.ReservasMed;
+
+
+/**
+ *
+ * @author Joao
+ */
+public class CentroMedico {
+
+    public static void main(String[] args) {
+        
+        PrincipalCenter prin = new PrincipalCenter();
+        prin.setVisible(true);
+        prin.setLocationRelativeTo(null);
+        
+        IngresoDatos ing = new IngresoDatos();
+        ing.setLocationRelativeTo(null);
+        
+        ReservasMed reser = new ReservasMed();
+        reser.setLocationRelativeTo(null);
+        
+       
+
+        
+        
+
+
+       
+    }
+  
+     
+    
+}
